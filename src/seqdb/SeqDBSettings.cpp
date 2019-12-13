e6ad582236e6bf48128ea2b37d384db5f7aeb14f
Ivan Sovic <isovic@pacificbiosciences.com>
2019-12-13 18:18:48 +0100

    Added options to the CLI, and implemented a simple workflow which creates the DB.

--- src/seqdb/SeqDBSettings.cpp
+++ src/seqdb/SeqDBSettings.cpp
@@ -2,6 +2,7 @@
 
 #include "seqdb/SeqDBSettings.h"
 #include <pacbio/Version.h>
+#include <limits>
 
 namespace PacBio {
 namespace Pancake {
@@ -34,11 +35,18 @@ R"({
     "type" : "int"
 })", SeqDBSettings::Defaults::CompressionLevel};
 
+const CLI_v2::Option BufferSize{
+R"({
+    "names" : ["buffer-size"],
+    "description" : "Sequence buffer size in MB. Has to be >= 0.0.",
+    "type" : "float"
+})", SeqDBSettings::Defaults::BufferSize};
+
 const CLI_v2::Option BlockSize{
 R"({
     "names" : ["block-size"],
-    "description" : "Block size in MB. Value 0 means all sequences will be in one block.",
-    "type" : "int"
+    "description" : "Block size in MB. Value 0 means one sequnece per block, value < 0 all sequences in one block.",
+    "type" : "float"
 })", SeqDBSettings::Defaults::BlockSize};
 
 // clang-format on
@@ -53,6 +61,7 @@ SeqDBSettings::SeqDBSettings(const PacBio::CLI_v2::Results& options)
     , IsFofn{options[OptionNames::IsFofn]}
     , NumThreads{options.NumThreads()}
     , CompressionLevel{options[OptionNames::CompressionLevel]}
+    , BufferSize{options[OptionNames::BufferSize]}
     , BlockSize{options[OptionNames::BlockSize]}
 {
     // Allow multiple positional input arguments.
@@ -63,6 +72,18 @@ SeqDBSettings::SeqDBSettings(const PacBio::CLI_v2::Results& options)
     InputFiles.clear();
     for (size_t i = 1; i < files.size(); ++i)
         InputFiles.push_back(files[i]);
+
+    // Convert block and buffer sizes from MB to bytes.
+    BlockSize *= (1024 * 1024);
+    BufferSize *= (1024 * 1024);
+
+    // Negative block size indicates that everything should be in one block.
+    if (BlockSize < 0.0f)
+        BlockSize = std::numeric_limits<int64_t>::max() / (1024.0f * 1024.0f);
+
+    // Buffer size can be zero, but not negative.
+    if (BufferSize < 0.0f)
+        throw std::runtime_error("Buffer size cannot be a negative value.");
 }
 
 PacBio::CLI_v2::Interface SeqDBSettings::CreateCLI()
@@ -74,6 +95,7 @@ PacBio::CLI_v2::Interface SeqDBSettings::CreateCLI()
     i.AddOptionGroup("Algorithm Options", {
         OptionNames::IsFofn,
         OptionNames::CompressionLevel,
+        OptionNames::BufferSize,
         OptionNames::BlockSize,
     });
     i.AddPositionalArguments({
