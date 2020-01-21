6052f4ad6ed16e3cbff50b99eddb2301b3ba5e13
Ivan Sovic <isovic@pacificbiosciences.com>
2020-01-21 10:59:13 +0100

    Implemented the SeedDBReader.

---
+++ include/pacbio/seeddb/SeedDBReader.h
@@ -0,0 +1,59 @@
+// Author: Ivan Sovic
+
+#ifndef PANCAKE_SEEDDB_READER_H
+#define PANCAKE_SEEDDB_READER_H
+
+#include <pacbio/seeddb/SeedDBIndexCache.h>
+#include <pacbio/seeddb/SequenceSeeds.h>
+#include <pacbio/seqdb/Util.h>
+#include <memory>
+#include <ostream>
+#include <string>
+
+namespace PacBio {
+namespace Pancake {
+
+class SeedDBReader
+{
+public:
+    SeedDBReader(std::shared_ptr<PacBio::Pancake::SeedDBIndexCache>& seedDBCache);
+    ~SeedDBReader();
+
+    bool GetSeedsForSequence(SequenceSeeds& record, int64_t seqId);
+    bool GetSeedsForSequence(SequenceSeeds& record, const std::string& seqName);
+    bool GetNext(SequenceSeeds& record);
+
+    bool GetNextBatch(std::vector<SequenceSeeds>& records, int64_t batchSize);
+    bool GetBlock(std::vector<SequenceSeeds>& records, int32_t blockId);
+
+    bool JumpTo(int64_t seqId);
+    bool JumpTo(const std::string& seqName);
+
+private:
+    using FilePtr = std::unique_ptr<FILE, FileDeleter>;
+    class OpenFileHandler
+    {
+    public:
+        FilePtr fp = nullptr;
+        int32_t fileId = -1;
+        int64_t pos = 0;
+        int32_t nextOrdinalId = 0;
+    };
+    std::shared_ptr<PacBio::Pancake::SeedDBIndexCache> seedDBIndexCache_;
+    OpenFileHandler fileHandler_;
+
+    void AccessLocation_(OpenFileHandler& fileHandler,
+                         const std::vector<PacBio::Pancake::SeedDBFileLine>& fileLines,
+                         const std::string& indexParentFolder, int32_t fileId,
+                         int32_t nextOrdinalId, int64_t offset) const;
+
+    void LoadSeedsForSequence_(Pancake::SequenceSeeds& record, OpenFileHandler& fileHandler,
+                               const std::vector<PacBio::Pancake::SeedDBFileLine>& fileLines,
+                               const std::string& indexParentFolder, const SeedDBSeedsLine& sl,
+                               int32_t ordinalId) const;
+};
+
+}  // namespace Pancake
+}  // namespace PacBio
+
+#endif  // PANCAKE_SEQDB_READER_H
