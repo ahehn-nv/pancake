dc86e47272c927e91658cfedc79c00e8b94325cf
Ivan Sovic <isovic@pacificbiosciences.com>
2020-01-21 10:35:02 +0100

    Added a new SequenceSeeds class to hold seeds for one particular sequence.

---
+++ include/pacbio/seeddb/SequenceSeeds.h
@@ -0,0 +1,101 @@
+// File Description
+/// \file Sequence.h
+/// \brief Defines the SequenceSeeds class. It contains all seeds for a given sequence,
+//         tied with the sequence ID and length.
+//
+// Author: Ivan Sovic
+
+#ifndef PANCAKE_SEQUENCE_SEEDS_H
+#define PANCAKE_SEQUENCE_SEEDS_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace PacBio {
+namespace Pancake {
+
+///
+/// \brief The SequenceSeeds class hold seeds for a given sequence.
+///
+class SequenceSeeds
+{
+public:
+    /// \name Constructors & Related Methods
+    /// \{
+
+    ///
+    /// \brief Sequence
+    /// \param name
+    /// \param seeds
+    /// \param id
+    ///
+    explicit SequenceSeeds(std::string name, std::vector<__int128> seeds, int64_t id);
+
+    SequenceSeeds() = default;
+
+    /// \}
+
+public:
+    /// \name Attributes
+    /// \{
+
+    ///
+    /// \brief Name
+    /// \return
+    ///
+    const std::string& Name() const;
+
+    ///
+    /// \brief
+    ///
+    /// \param name
+    /// \return SequenceSeeds&
+    ///
+    SequenceSeeds& Name(std::string name);
+
+    ///
+    /// \brief Seeds
+    /// \return
+    ///
+    const std::vector<__int128>& Seeds() const;
+
+    ///
+    /// \brief
+    ///
+    /// \param seeds
+    /// \return SequenceSeeds&
+    ///
+    SequenceSeeds& Seeds(std::vector<__int128> seeds);
+
+    ///
+    /// \brief
+    ///
+    /// \param id
+    /// \return int64_t
+    ///
+    int64_t Id() const;
+
+    ///
+    /// \brief
+    ///
+    /// \param id
+    /// \return int64_t
+    ///
+    SequenceSeeds& Id(int64_t id);
+
+    /// \}
+
+    bool operator==(const SequenceSeeds& other) const;
+    bool operator!=(const SequenceSeeds& other) const;
+
+private:
+    std::string name_;
+    std::vector<__int128> seeds_;
+    int64_t id_;
+};
+
+}  // namespace BAM
+}  // namespace PacBio
+
+#endif  // PANCAKE_SEQUENCE_SEEDS_H
