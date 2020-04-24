fa7ced362b465515109a453a353c72fd1f693f8d
Ivan Sovic <ivan.sovic@gmail.com>
2020-04-24 12:40:09 +0200

    Added Doxygen to include/pacbio/util/FileIO.h.

--- include/pacbio/util/FileIO.h
+++ include/pacbio/util/FileIO.h
@@ -11,6 +11,7 @@
 namespace PacBio {
 namespace Pancake {
 
+/// \brief Enum class with all supported input sequence formats.
 enum class SequenceFormat
 {
     Fasta,
@@ -22,18 +23,37 @@ enum class SequenceFormat
     Unknown,
 };
 
+/// \brief Parses a file line by line, and returns a vector of strings, one per line.
 std::vector<std::string> LoadLinesToVector(const std::string& listPath);
+
+/// \brief Parses a file line by line, and stores each line into a set.
+///         Useful for parsing sets of reads or similar identifiers.
 std::unordered_set<std::string> LoadLinesToSet(const std::string& listPath);
 
+/// \brief Checks if the provided file path has a FASTA/gzipped FASTA type of extension.
 bool FormatIsFasta(const std::string& fn);
+
+/// \brief Checks if the provided file path has a FASTQ/gzipped FASTQ type of extension.
 bool FormatIsFastq(const std::string& fn);
+
+/// \brief Checks if the provided file path has a FOFN extension.
 bool FormatIsFofn(const std::string& fn);
+
+/// \brief Checks if the provided file path has a BAM extension.
 bool FormatIsBam(const std::string& fn);
+
+/// \brief Checks if the provided file path has an XML extension.
 bool FormatIsXml(const std::string& fn);
+
+/// \brief Checks if the provided file path has a SeqDBe xtension.
 bool FormatIsSeqDB(const std::string& fn);
 
+/// \brief Parses the format of a file based on it's extension.
 SequenceFormat ParseFormat(const std::string& filename);
 
+/// \brief Recursively expands all composite files in the list (FOFN, XML) until
+///         there are no more composite files left.
+/// \returns Vector of pairs of final non-composite files and their file formats.
 std::vector<std::pair<SequenceFormat, std::string>> ExpandInputFileList(
     const std::vector<std::string>& inFiles);
 
