8a3b6478d08c3956cd4f664caf414df1a4adc4e0
Ivan Sovic <ivan.sovic@gmail.com>
2020-04-24 16:19:46 +0200

    Implemented new RunLengthEncoding functions: (I) A set of functions (like already existing ones) which compute the coordinates for converting to-and-from the HPC space, and (II) a function which stores the output HPC in a new vector and dynamically resizes the vectors only if their size is smaller than required. Vectors can be larger than the resulting hpcLen, and the hpcLen should be taken as the actual length of the data. This is intended to provide a scratch space for fast computation and to avoid reallocation as much as possible.

--- include/pacbio/util/RunLengthEncoding.h
+++ include/pacbio/util/RunLengthEncoding.h
@@ -23,6 +23,36 @@ void RunLengthEncoding(const char* seq, int64_t seqLen, std::string& encodedSeq,
 /// \brief Compresses the homopolymers from the input sequence in-place.
 int64_t RunLengthEncoding(char* seq, int64_t seqLen, std::vector<int32_t>& runLengths);
 
+/// \brief Compresses the homopolymers from the input sequence. Returns (via
+///         parameters) the compressed sequence and two vectors for converting the
+///         positions from the compressed space back to the original sequence
+///         space (hpcToSeqCoords) and vice versa (seqToHPCCoords).
+void RunLengthEncoding(const std::string& seq, std::string& encodedSeq,
+                       std::vector<int32_t>& seqToHPCCoords, std::vector<int32_t>& hpcToSeqCoords);
+
+/// \brief Compresses the homopolymers from the input sequence. Returns (via
+///         parameters) the compressed sequence and two vectors for converting the
+///         positions from the compressed space back to the original sequence
+///         space (hpcToSeqCoords) and vice versa (seqToHPCCoords).
+void RunLengthEncoding(const char* seq, int64_t seqLen, std::string& encodedSeq,
+                       std::vector<int32_t>& seqToHPCCoords, std::vector<int32_t>& hpcToSeqCoords);
+
+/// \brief Compresses the homopolymers from the input sequence in place. Returns (via
+///         parameters) the compressed sequence and two vectors for converting the
+///         positions from the compressed space back to the original sequence
+///         space (hpcToSeqCoords) and vice versa (seqToHPCCoords).
+int64_t RunLengthEncoding(char* seq, int64_t seqLen, std::vector<int32_t>& seqToHPCCoords,
+                          std::vector<int32_t>& hpcToSeqCoords);
+
+/// \brief Specialized function which should be used carefuly! The output buffers:
+///         destHPC, seqToHPCCoords and hpcToSeqCoords will always be larger than the
+///         length of the HPC sequence to optimize the number of reallocations.
+//          That's why the "hpcLen" output value should be used to determine how
+//          many elements are valid in destHPC and hpcToSeqCoords.
+int64_t RunLengthEncoding(const char* seq, int64_t seqLen, std::vector<char>& destHPC,
+                          int32_t& hpcLen, std::vector<int32_t>& seqToHPCCoords,
+                          std::vector<int32_t>& hpcToSeqCoords);
+
 }  // namespace Pancake
 }  // namespace PacBio
 
--- src/util/RunLengthEncoding.cpp
+++ src/util/RunLengthEncoding.cpp
@@ -48,5 +48,94 @@ int64_t RunLengthEncoding(char* seq, int64_t seqLen, std::vector<int32_t>& runLe
     return prev;
 }
 
+void RunLengthEncoding(const std::string& seq, std::string& encodedSeq,
+                       std::vector<int32_t>& seqToHPCCoords, std::vector<int32_t>& hpcToSeqCoords)
+{
+    RunLengthEncoding(seq.c_str(), seq.size(), encodedSeq, seqToHPCCoords, hpcToSeqCoords);
+}
+
+void RunLengthEncoding(const char* seq, int64_t seqLen, std::string& encodedSeq,
+                       std::vector<int32_t>& seqToHPCCoords, std::vector<int32_t>& hpcToSeqCoords)
+{
+    encodedSeq = std::string(seq, seqLen);
+    seqToHPCCoords.clear();
+    hpcToSeqCoords.clear();
+    if (seq == NULL || seqLen == 0) return;
+
+    int64_t outSeqLen = RunLengthEncoding(&encodedSeq[0], seqLen, seqToHPCCoords, hpcToSeqCoords);
+    encodedSeq[outSeqLen] = '\n';
+    encodedSeq.resize(outSeqLen);
+}
+
+int64_t RunLengthEncoding(char* seq, int64_t seqLen, std::vector<int32_t>& seqToHPCCoords,
+                          std::vector<int32_t>& hpcToSeqCoords)
+{
+    seqToHPCCoords.clear();
+    hpcToSeqCoords.clear();
+    if (seq == NULL || seqLen == 0) return 0;
+
+    seqToHPCCoords.resize(seqLen, 0);
+    hpcToSeqCoords.resize(seqLen, 0);
+    int64_t prev = 0;
+    int64_t count = 0;
+
+    for (int64_t i = 0; i < seqLen; ++i) {
+        if (seq[prev] != seq[i]) {
+            ++prev;
+            count = 0;
+            seq[prev] = seq[i];
+        }
+        ++count;
+        seqToHPCCoords[i] = prev;
+        hpcToSeqCoords[prev] = i;
+    }
+
+    ++prev;
+    hpcToSeqCoords.resize(prev);
+
+    return prev;
+}
+
+int64_t RunLengthEncoding(const char* seq, int64_t seqLen, std::vector<char>& destHPC,
+                          int32_t& hpcLen, std::vector<int32_t>& seqToHPCCoords,
+                          std::vector<int32_t>& hpcToSeqCoords)
+{
+    if (seq == NULL || seqLen == 0) return 0;
+
+    // All buffers should have the same length for speed.
+    // If not, clear them so that the below if can only compare one of them.
+    if (seqToHPCCoords.size() != hpcToSeqCoords.size() || destHPC.size() != seqToHPCCoords.size() ||
+        destHPC.size() != hpcToSeqCoords.size()) {
+        destHPC.clear();
+        seqToHPCCoords.clear();
+        hpcToSeqCoords.clear();
+    }
+    if (seqLen > static_cast<int64_t>(destHPC.size())) {
+        // Reduce the amount of resizing. Only increase the size of the buffer when required.
+        destHPC.resize(seqLen);
+        seqToHPCCoords.resize(seqLen);
+        hpcToSeqCoords.resize(seqLen);
+    }
+
+    int64_t prev = 0;
+    int64_t count = 0;
+    char* destRaw = &destHPC[0];
+
+    for (int64_t i = 0; i < seqLen; ++i) {
+        if (destRaw[prev] != seq[i]) {
+            ++prev;
+            count = 0;
+            destRaw[prev] = seq[i];
+        }
+        ++count;
+        seqToHPCCoords[i] = prev;
+        hpcToSeqCoords[prev] = i;
+    }
+
+    hpcLen = prev;
+
+    return prev;
+}
+
 }  // namespace Pancake
 }  // namespace PacBio
