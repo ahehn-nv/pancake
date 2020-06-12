fb940a4970c0eca6a6d19da96bb74797711e7315
Ivan Sovic <ivan.sovic@gmail.com>
2020-06-12 13:11:15 +0200

    Added tests for ValidateCigar, in tests/src/test_AlignmentTools.cpp.

--- tests/src/test_AlignmentTools.cpp
+++ tests/src/test_AlignmentTools.cpp
@@ -31,4 +31,51 @@ TEST(Test_EdlibAlignmentTools, SimpleTest)
     PacBio::BAM::Cigar result = EdlibAlignmentToCigar(input.data(), input.size());
     EXPECT_EQ(expected, result);
 }
+
+TEST(Test_AlignmentTools_ValidateCigar, ArrayOfTests)
+{
+    // clang-format off
+    std::vector<std::tuple<std::string, std::string, std::string, std::string, bool>> testData = {
+        // Good cases.
+        {"Empty input", "", "", "", false},
+        {"Exact match, good", "ACTG", "ACTG", "4=", false},
+        {"Cigar with indels", "ACTT", "ACTG", "3=1I1D", false},
+        {"Soft clipping", "AAAACTTAAA", "ACTG", "3S3=1X3S", false},
+        {"Hard clipping", "ACTT", "ACTG", "3H3=1X3H", false},
+        {"Simple test case", "GGATCAGTTTTATATACAC", "GAGTTCGTTCTATATATACAC", "1=1I1=1D1=1D1=1I3=1X3=2D4=2I", false},
+
+        // Bad cases.
+        {"One base is incorrect", "ACTT", "ACTG", "4=", true},
+        {"Length mismatch", "ACT", "ACTG", "4=", true},
+        {"Non-zero CIGAR, but empty seqs", "", "", "4=", true},
+        {"Length mismatch with indels", "ACTT", "ACTG", "3=1I1D1I", true},
+        {"Length mismatch with mismatches", "ACTT", "ACTG", "3=2X", true},
+    };
+    // clang-format on
+
+    for (const auto& data : testData) {
+        // Inputs.
+        const std::string testName = std::get<0>(data);
+        const std::string& query = std::get<1>(data);
+        const std::string& target = std::get<2>(data);
+        PacBio::BAM::Cigar cigar(std::get<3>(data));
+        bool shouldThrow = std::get<4>(data);
+
+        // Name the test.
+        SCOPED_TRACE("ValidateCigar-" + testName);
+
+        // Run.
+        if (shouldThrow) {
+            EXPECT_THROW(
+                {
+                    PacBio::Pancake::ValidateCigar(query.c_str(), query.size(), target.c_str(),
+                                                   target.size(), cigar, "");
+                },
+                std::runtime_error);
+        } else {
+            PacBio::Pancake::ValidateCigar(query.c_str(), query.size(), target.c_str(),
+                                           target.size(), cigar, "");
+        }
+    }
+}
 }
