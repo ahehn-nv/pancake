89276bd450e6fd58bb54ce73b39f7ea4a75ae0ba
Ivan Sovic <isovic@pacificbiosciences.com>
2020-11-05 14:32:15 +0100

    SeedHit now stores the span.

--- include/pacbio/pancake/Seed.h
+++ include/pacbio/pancake/Seed.h
@@ -22,77 +22,90 @@ namespace SeedDB {
 static const int8_t MINIMIZER_FLAG_DEFAULT_FWD = 0x00;
 static const int8_t MINIMIZER_FLAG_IS_REV = 0x01;
 static const PacBio::Pancake::Int128t MINIMIZER_CODED_REV_BIT =
-    (((PacBio::Pancake::Int128t)1) << 32);
+    (static_cast<PacBio::Pancake::Int128t>(1) << 32);
 
-static const PacBio::Pancake::Int128t MINIMIZER_64bit_MASK =
-    (((PacBio::Pancake::Int128t)0x0FFFFFFFFFFFFFFFF));
-static const PacBio::Pancake::Int128t MINIMIZER_32bit_MASK =
-    (((PacBio::Pancake::Int128t)0x0000000007FFFFFFF));
-static const PacBio::Pancake::Int128t MINIMIZER_32bit_MASK_FULL =
-    (((PacBio::Pancake::Int128t)0x000000000FFFFFFFF));
+static const PacBio::Pancake::Int128t MINIMIZER_64bit_MASK = 0x0FFFFFFFFFFFFFFFF;
+static const PacBio::Pancake::Int128t MINIMIZER_32bit_MASK = 0x0000000007FFFFFFF;
+static const PacBio::Pancake::Int128t MINIMIZER_32bit_MASK_FULL = 0x000000000FFFFFFFF;
+static const PacBio::Pancake::Int128t MINIMIZER_8bit_MASK = 0x000000000000000FF;
+static const PacBio::Pancake::Int128t MINIMIZER_1bit_MASK = 0x00000000000000001;
 
 using SeedRaw = PacBio::Pancake::Int128t;
 
 class Seed
 {
 public:
-    Seed() : key(0), seqID(0), pos(0), flag(0) {}
+    Seed() : key(0), span(0), seqID(0), seqRev(0), pos(0) {}
 
     Seed(uint64_t _key, int32_t _seqID, int32_t _pos, bool _isRev)
-        : key(_key)
-        , seqID(_seqID)
-        , pos(_pos)
-        , flag((_isRev) ? MINIMIZER_FLAG_IS_REV : MINIMIZER_FLAG_DEFAULT_FWD)
+        : key(_key), span(0), seqID(_seqID), seqRev(_isRev), pos(_pos)
     {
+    }
+
+    Seed(uint64_t _key, int32_t _span, int32_t _seqID, int32_t _pos, bool _isRev)
+        : key(_key), span(_span), seqID(_seqID), seqRev(_isRev), pos(_pos)
+    {
+        if (_span >= 256) {
+            throw std::runtime_error(
+                "Seed span is larger than 8-bits. Can't encode that. (In Seed constructor.)");
+        }
     }
     Seed(const PacBio::Pancake::Int128t& codedKeypos)
         : key(DecodeKey(codedKeypos))
+        , span(DecodeSpan(codedKeypos))
         , seqID(DecodeSeqId(codedKeypos))
+        , seqRev(DecodeIsRev(codedKeypos))
         , pos(DecodePos(codedKeypos))
-        , flag(DecodeIsRev(codedKeypos))
     {
     }
 
-    bool IsRev() { return (flag & MINIMIZER_FLAG_IS_REV); }
+    bool IsRev() { return seqRev; }
 
     inline PacBio::Pancake::Int128t To128t()
     {
-        PacBio::Pancake::Int128t ret = ((PacBio::Pancake::Int128t)key) << 64;
-        ret |= ((((PacBio::Pancake::Int128t)(seqID)) << 1) & MINIMIZER_32bit_MASK) << 32;
-        ret |= ((PacBio::Pancake::Int128t)(pos)) & MINIMIZER_32bit_MASK;
-
-        if (flag & MINIMIZER_FLAG_IS_REV) {
-            ret |= MINIMIZER_CODED_REV_BIT;
-        }
-
+        PacBio::Pancake::Int128t ret = static_cast<PacBio::Pancake::Int128t>(key) << (64 + 8);
+        ret |= (static_cast<PacBio::Pancake::Int128t>(span) & MINIMIZER_8bit_MASK) << 64;
+        ret |= ((static_cast<PacBio::Pancake::Int128t>(seqID) << 1) & MINIMIZER_32bit_MASK) << 32;
+        ret |= (static_cast<PacBio::Pancake::Int128t>(seqRev) & MINIMIZER_1bit_MASK) << 32;
+        ret |= static_cast<PacBio::Pancake::Int128t>(pos) & MINIMIZER_32bit_MASK;
         return ret;
     }
 
     static inline PacBio::Pancake::Int128t Encode(uint64_t _key, int32_t _seqID, int32_t _pos,
                                                   bool _isRev)
     {
-        PacBio::Pancake::Int128t ret = ((PacBio::Pancake::Int128t)_key) << 64;
-        ret |= (((PacBio::Pancake::Int128t)(_seqID)) & MINIMIZER_32bit_MASK) << 33;
-        ret |= ((PacBio::Pancake::Int128t)(_pos)) & MINIMIZER_32bit_MASK;
-        if (_isRev) {
-            ret |= MINIMIZER_CODED_REV_BIT;
-        }
+        return Encode(_key, 0, _seqID, _pos, _isRev);
+    }
+
+    static inline PacBio::Pancake::Int128t Encode(uint64_t _key, int32_t _span, int32_t _seqID,
+                                                  int32_t _pos, bool _isRev)
+    {
+        PacBio::Pancake::Int128t ret = static_cast<PacBio::Pancake::Int128t>(_key) << 72;
+        ret |= (static_cast<PacBio::Pancake::Int128t>(_span) & MINIMIZER_8bit_MASK) << 64;
+        ret |= ((static_cast<PacBio::Pancake::Int128t>(_seqID) << 1) & MINIMIZER_32bit_MASK) << 32;
+        ret |= (static_cast<PacBio::Pancake::Int128t>(_isRev) & MINIMIZER_1bit_MASK) << 32;
+        ret |= static_cast<PacBio::Pancake::Int128t>(_pos) & MINIMIZER_32bit_MASK;
         return ret;
     }
 
     static inline uint64_t DecodeKey(const PacBio::Pancake::Int128t& seed)
     {
-        return (uint64_t)((seed >> 64) & MINIMIZER_64bit_MASK);
+        return ((seed >> (64 + 8)) & MINIMIZER_64bit_MASK);
+    }
+
+    static inline uint64_t DecodeSpan(const PacBio::Pancake::Int128t& seed)
+    {
+        return ((seed >> 64) & MINIMIZER_8bit_MASK);
     }
 
     static inline int32_t DecodePos(const PacBio::Pancake::Int128t& seed)
     {
-        return ((int32_t)(seed & MINIMIZER_32bit_MASK));
+        return (seed & MINIMIZER_32bit_MASK);
     }
 
     static inline int32_t DecodeSeqId(const PacBio::Pancake::Int128t& seed)
     {
-        return (int32_t)((seed >> 33) & MINIMIZER_32bit_MASK);
+        return ((seed >> 33) & MINIMIZER_32bit_MASK);
     }
 
     /*
@@ -102,7 +115,7 @@ public:
      */
     static inline int32_t DecodeSeqIdWithRev(const PacBio::Pancake::Int128t& seed)
     {
-        return (int32_t)((seed >> 32) & MINIMIZER_32bit_MASK);
+        return ((seed >> 32) & MINIMIZER_32bit_MASK);
     }
 
     static inline bool DecodeIsRev(const PacBio::Pancake::Int128t& seed)
@@ -113,14 +126,15 @@ public:
     std::string Verbose() const
     {
         std::stringstream ss;
-        ss << "pos = " << pos << ", seqID = " << seqID << ", flag = " << (int32_t)flag
-           << ", key = " << key;
+        ss << "pos = " << pos << ", span = " << span << ", seqID = " << seqID
+           << ", seqRev = " << seqRev << ", key = " << key;
         return ss.str();
     }
 
     inline int32_t Compare(const Seed& other) const
     {
-        if (key == other.key && seqID == other.seqID && pos == other.pos && flag == other.flag) {
+        if (key == other.key && span == other.span && seqID == other.seqID && pos == other.pos &&
+            seqRev == other.seqRev) {
             // Exact seed match.
             return 0;
         } else if (key != other.key) {
@@ -131,10 +145,11 @@ public:
         return 2;
     }
 
-    uint64_t key;
-    int32_t seqID;
+    uint64_t key : 56;
+    uint64_t span : 8;
+    int32_t seqID : 31;
+    bool seqRev : 1;
     int32_t pos;
-    int8_t flag;
 };
 
 }  // namespace SeedDB
