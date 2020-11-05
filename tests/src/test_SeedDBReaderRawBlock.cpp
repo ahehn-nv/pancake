08eed980aea22c6cf7ed746640acb342c193c19a
Ivan Sovic <isovic@pacificbiosciences.com>
2020-11-05 19:12:07 +0100

    Updated unit tests in tests/src/test_SeedDBReaderRawBlock.cpp, two SeedDB caches needed to be updated to the new kmer size.

--- tests/src/test_SeedDBReaderRawBlock.cpp
+++ tests/src/test_SeedDBReaderRawBlock.cpp
@@ -253,23 +253,25 @@ TEST(SeedDBReaderRawBlock, GetBlock_NormalWithMultipleFiles)
      * This will load a block with the new function and return a set of seeds.
      * To verify, each of the sequences in the block will be loaded with the
      * previously tested reader and accumulated, then compared.
+     *
+     * The difference between this SeedDB specified here in the string and the one
+     * stored in "test-data/seeddb-writer/test-1b.seeddb" is only in the block groupings.
     */
-
     const std::string inSeedDB =
         R"(V	0.1.0
-P	k=30,w=80,hpc=0,hpc_len=10,rc=1
-F	0	test-1b.seeddb.0.seeds	1	2976
-F	1	test-1b.seeddb.1.seeds	1	7664
-F	2	test-1b.seeddb.2.seeds	1	12384
-F	3	test-1b.seeddb.3.seeds	1	2608
-F	4	test-1b.seeddb.4.seeds	1	10304
-S	0	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3005/0_5852	0	0	2976	5852	186
-S	1	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3414/0_11983	1	0	7664	11983	479
-S	2	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3820/0_24292	2	0	12384	24292	774
-S	3	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3981/0_5105	3	0	2608	5105	163
-S	4	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/4028/0_19001	4	0	10304	19001	644
+P	k=20,w=80,s=0,hpc=0,hpc_len=10,rc=1
+F	0	test-1b.seeddb.0.seeds	1	2464
+F	1	test-1b.seeddb.1.seeds	1	4752
+F	2	test-1b.seeddb.2.seeds	1	9488
+F	3	test-1b.seeddb.3.seeds	1	1904
+F	4	test-1b.seeddb.4.seeds	1	7456
+S	0	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3005/0_5852	0	0	2464	5852	154
+S	1	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3414/0_11983	1	0	4752	11983	297
+S	2	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3820/0_24292	2	0	9488	24292	593
+S	3	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3981/0_5105	3	0	1904	5105	119
+S	4	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/4028/0_19001	4	0	7456	19001	466
 B	0	0	1	2976
-B	1	1	5	32960
+B	1	1	5	23600
 )";
     const int32_t blockId = 1;
 
@@ -277,7 +279,7 @@ B	1	1	5	32960
     std::istringstream is(inSeedDB);
     std::shared_ptr<PacBio::Pancake::SeedDBIndexCache> seedDBCache =
         PacBio::Pancake::LoadSeedDBIndexCache(
-            is, PacBio::PancakeTestsConfig::Data_Dir + "/seeddb-writer/test-1.seeddb");
+            is, PacBio::PancakeTestsConfig::Data_Dir + "/seeddb-writer/test-1b.seeddb");
 
     // Expected results. Construct these by loading and flattening the block using
     // the SeedDBReader.
@@ -313,12 +315,13 @@ TEST(SeedDBReaderRawBlock, GetBlock_NormalWithSingleFileAndAGap)
 
     const std::string inSeedDB =
         R"(V	0.1.0
-F	0	test-1a.seeddb.0.seeds	4	23552
-S	0	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3005/0_5852	0	0	2976	5852	186
-S	1	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3414/0_11983	0	2976	7664	11983	479
-S	2	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3981/0_5105	0	23024	2608	5105	163
-S	3	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/4028/0_19001	0	25632	10304	19001	644
-B	0	0	4	23552
+P	k=20,w=80,s=0,hpc=0,hpc_len=10,rc=1
+F	0	test-1a.seeddb.0.seeds	5	26064
+S	0	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3005/0_5852	0	0	2464	5852	154
+S	1	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3414/0_11983	0	2464	4752	11983	297
+S	2	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/3981/0_5105	0	16704	1904	5105	119
+S	3	m141013_011508_sherri_c100709962550000001823135904221533_s1_p0/4028/0_19001	0	18608	7456	19001	466
+B	0	0	4	16576
 )";
     const int32_t blockId = 0;
 
