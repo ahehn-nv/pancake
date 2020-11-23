930ed0e8728fab4223205b592450d80b1e8d0c54
Ivan Sovic <ivan.sovic@gmail.com>
2020-11-23 19:02:30 +0100

    Cleanup in AlignerKSW2.*.

--- include/pacbio/pancake/AlignerKSW2.h
+++ include/pacbio/pancake/AlignerKSW2.h
@@ -46,8 +46,6 @@ private:
     AlignmentParameters opt_;
     Minimap2ThreadBufferPtr buffer_;
     int8_t mat_[25];
-    // mm_idxopt_t iopt_;
-    // mm_mapopt_t mopt_;
 
     void ConvertMinimap2CigarToPbbam_(uint32_t* mm2Cigar, int32_t cigarLen,
                                       const std::vector<uint8_t>& qseq,
@@ -58,13 +56,11 @@ private:
     static std::vector<uint8_t> ConvertSeqAlphabet_(const char* seq, size_t seqlen,
                                                     const int8_t* conv_table);
 
-    static void ksw_gen_simple_mat_(int m, int8_t* mat, int8_t a, int8_t b, int8_t sc_ambi);
-    // static int mm_test_zdrop_(void* km, const mm_mapopt_t* opt, const uint8_t* qseq,
-    //                           const uint8_t* tseq, uint32_t n_cigar, uint32_t* cigar,
-    //                           const int8_t* mat);
-    static void mm_align_pair_(void* km, int qlen, const uint8_t* qseq, int tlen,
-                               const uint8_t* tseq, const int8_t* mat, int w, int end_bonus,
-                               int zdrop, int flag, ksw_extz_t* ez, int q, int e, int q2, int e2);
+    static void GenerateSimpleMatrix_(int m, int8_t* mat, int8_t a, int8_t b, int8_t scAmbi);
+
+    static void AlignPair_(void* km, int qlen, const uint8_t* qseq, int tlen, const uint8_t* tseq,
+                           const int8_t* mat, int w, int endBonus, int zdrop, int flag,
+                           ksw_extz_t* ez, int q, int e, int q2, int e2);
 };
 
 }  // namespace Pancake
