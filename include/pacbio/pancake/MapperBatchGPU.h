146f11a4df83dbbb8cf92f7cb81d421c34c34043
Ivan Sovic <ivan.sovic@gmail.com>
2021-02-09 14:43:52 -0800

    Implemented the MapperBatchGPU. It runs mapping in parallel on the CPU, aligns on the GPU and then stitches the alignments back on the CPU. GPU alignment can have multiple fallbacks for the bandwidth, including a fallback to align the remaining parts on the CPU.

---
+++ include/pacbio/pancake/MapperBatchGPU.h
@@ -0,0 +1,63 @@
+// Author: Ivan Sovic
+
+#ifndef PANCAKE_MAPPER_BATCH_GPU_H
+#define PANCAKE_MAPPER_BATCH_GPU_H
+
+#include <pacbio/pancake/AlignerBatchGPU.h>
+#include <pacbio/pancake/FastaSequenceCached.h>
+#include <pacbio/pancake/MapperBase.h>
+#include <pacbio/pancake/MapperBatchCPU.h>
+#include <pacbio/pancake/MapperBatchUtility.h>
+#include <pacbio/pancake/MapperCLR.h>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace PacBio {
+namespace Pancake {
+
+class MapperBatchGPU
+{
+public:
+    MapperBatchGPU(const MapperCLRSettings& settings, int32_t numThreads, int32_t gpuStartBandwidth,
+                   int32_t gpuMaxBandwidth, uint32_t gpuDeviceId, double gpuMaxFreeMemoryFraction,
+                   int64_t gpuMaxMemoryCap, bool alignRemainingOnCpu);
+    ~MapperBatchGPU();
+
+    std::vector<std::vector<MapperBaseResult>> MapAndAlign(
+        const std::vector<MapperBatchChunk>& batchData);
+
+private:
+    MapperCLRSettings settings_;
+    int32_t numThreads_;
+    int32_t gpuStartBandwidth_;
+    int32_t gpuMaxBandwidth_;
+    uint32_t gpuDeviceId_;
+    double gpuMaxFreeMemoryFraction_;
+    int64_t gpuMaxMemoryCap_;
+    cudaStream_t gpuStream_;
+    bool alignRemainingOnCpu_;
+    claraparabricks::genomeworks::DefaultDeviceAllocator deviceAllocator_;
+
+    static std::vector<std::vector<MapperBaseResult>> MapAndAlignImpl_(
+        const std::vector<MapperBatchChunk>& batchChunks, const MapperCLRSettings& settings,
+        int32_t numThreads, bool alignRemainingOnCpu, int32_t gpuStartBandwidth,
+        int32_t gpuMaxBandwidth, uint32_t gpuDeviceId, cudaStream_t& gpuStream,
+        claraparabricks::genomeworks::DefaultDeviceAllocator& deviceAllocator);
+
+    static void WorkerMapper_(const std::vector<MapperBatchChunk>& batchChunks, int32_t startId,
+                              int32_t endId, MapperCLR& mapper,
+                              std::vector<std::vector<MapperBaseResult>>& results);
+
+    static int32_t AlignPartsOnGPU_(
+        int32_t gpuMaxBandwidth, uint32_t gpuDeviceId, cudaStream_t& gpuStream,
+        claraparabricks::genomeworks::DefaultDeviceAllocator& deviceAllocator,
+        const AlignmentParameters& alnParamsGlobal, const std::vector<PairForBatchAlignment>& parts,
+        std::vector<AlignmentResult>& retInternalAlns);
+};
+
+}  // namespace Pancake
+}  // namespace PacBio
+
+#endif  // PANCAKE_MAPPER_BATCH_GPU_H
